81921fc047f879aa677f7cf566d02de7c437716a
Lee <artbasher@gmail.com>
2015-10-15 13:54:21 -0700

    fix setup.py to work with pypi

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup
 
-setup(name='Slow Tests',
+setup(name='SlowTests',
       entry_points={
           'nose.plugins.0.10': [
               'slowtests= slow_tests:SlowTests'
           ]
       },
+      author="Yellowbeard",
+      author_email="artbasher@gmail.com",
+      url="https://github.com/yellow-beard/slow_tests",
       )
 
